4a827ea5f5884dd566a50c07b529eb5193e16f38
Smriti Ranjan <120196059+smritiranjan65@users.noreply.github.com>
2024-02-06 12:37:57 +0530

    first commit

---
+++ mailer.js
@@ -0,0 +1,32 @@
+nodemailer = require('nodemailer');
+
+
+// service provider, credentials, handshaking
+
+    transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure:true,
+        auth:{
+            user: 'smritiranjan65@gmail.com',
+            pass: 'duim wiob izee hpba'
+        }
+    })
+
+
+//mail object
+mail = {
+    from: 'smritiranjan65@gmail.com',
+    to: 'tkumari930451@gmail.com',
+    subject: 'OTP',
+    text: `Your otp for signin is value`
+}
+
+//send mail
+transporter.sendMail(mail, (err, data) => {
+    if (err) {
+        console.log(err)
+    }else {
+        console.log(data)
+    }
+})
